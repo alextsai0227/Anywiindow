667f7608be5a10c839dc91c2ef84a106cc0e1fc4
Alex T <alextsai0227@gmail.com>
2016-07-12 14:15:30 +0800

    Place refactor before

---
+++ Timer/WindowCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WindowCollectionViewCell.swift
+//  Timer
+//
+//  Created by 蔡舜珵 on 2016/7/12.
+//  Copyright © 2016年 蔡舜珵. All rights reserved.
+//
+
+import UIKit
+
+class WindowCollectionViewCell: UICollectionViewCell {
+    
+}
