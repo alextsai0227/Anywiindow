73282ca942666acc526c645bdd71878ea1fe2170
Alex T <alextsai0227@gmail.com>
2016-07-13 15:25:10 +0800

    random to window

--- Timer/CityViewController.swift
+++ Timer/CityViewController.swift
@@ -68,15 +68,12 @@ class CityViewController: UIViewController,UICollectionViewDataSource,UICollecti
     @IBAction func backToLastViewController(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func randomWindow(sender: AnyObject) {
+        let viewController = storyboard?.instantiateViewControllerWithIdentifier("ViewController") as! ViewController
+            viewController.place = place
+            presentViewController(viewController, animated: true, completion: nil)
+        
     }
-    */
-
+    
+    
 }
